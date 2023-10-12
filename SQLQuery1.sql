0034ff0530738043498c0803bed2f5de5f3276bc
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-10-12 18:41:09 +0300

    Test

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,60 +1,73 @@
-CREATE TABLE Sizes
-(
-Id INT PRIMARY KEY IDENTITY,
-Length INT NOT NULL CHECK  (Length>=10 and Length<=25),
-RingRange DECIMAL(18,2) NOT NULL CHECK(RingRange >=1.5 AND RingRange <=7.5)
-)
+SELECT 
+AircraftId
+,Manufacturer
+,FlightHours
+,COUNT(fd.Id) AS FlightDestinationsCount
+,ROUND(AVG(fd.TicketPrice),2)  AS AvgPrice
+FROM Aircraft as a 
+INNER  JOIN FlightDestinations as fd ON fd.AircraftId=a.Id
+GROUP BY AircraftId,Manufacturer,FlightHours
+HAVING COUNT(fd.Id) >=2
+ORDER BY  COUNT(fd.Id) DESC, AircraftId
+  
 
-CREATE TABLE Tastes
-(
-Id INT PRIMARY KEY IDENTITY,
-TasteType VARCHAR(20) NOT NULL,
-TasteStrength VARCHAR(15) NOT NULL,
-ImageURL NVARCHAR(100)   NOT NULL
-)
 
-CREATE TABLE Brands
-(
-Id INT PRIMARY KEY IDENTITY,
-BrandName VARCHAR(30) UNIQUE NOT NULL,
-BrandDescription VARCHAR(MAX) 
-)
+   SELECT 
+  p.FullName,
+  COUNT(a.Id) as CountOfAircraft,
+  SUM(fd.TicketPrice) as TotalTicketPrice
+FROM 
+  Passengers as p
+INNER JOIN 
+  FlightDestinations as fd ON fd.PassengerId = p.Id
+INNER JOIN 
+  Aircraft as a ON a.Id = fd.AircraftId
+GROUP BY 
+  p.FullName
+HAVING 
+  COUNT(a.Id) > 1 AND SUBSTRING(p.FullName, 2, 1) = 'A'
+ORDER BY 
+  FullName;
 
-CREATE TABLE Cigars
-(
-Id INT  PRIMARY KEY IDENTITY,
-CigarName Varchar(80) NOT NULL,
-BrandId INT FOREIGN KEY REFERENCES Brands(Id) NOT NULL,
-TastId INT FOREIGN KEY REFERENCES Tastes(Id) NOT NULL,
-SizeId INT FOREIGN KEY REFERENCES Sizes(Id) NOT NULL,
-PriceForSingleCigar FLOAT NOT NULL,
-ImageURL NVARCHAR(100) NOT NULL
+   
+
+   SELECT 
+   AirportName,
+   Start AS DayTime,
+   TicketPrice,
+   p.FullName,
+   Manufacturer,
+   Model
+   From FlightDestinations AS fd
+   INNER JOIN Airports as a ON a.Id =fd.AirportId
+   INNER JOIN Passengers as p ON p.Id=fd.PassengerId
+   INNER JOIN Aircraft as ac ON ac.Id = fd.AircraftId
+   WHERE DATEPart(HOUR,Start) >=6.00 AND DATEPART(HOUR,Start) <=20.00
+   AND TicketPrice >2500
+   ORDER BY model
+
+   SELECT * FROM FlightDestinations
+   SELECT * FROM Airports
+   SELECT * FROM Aircraft
+
+   GO
+   CREATE function udf_FlightDestinationsByEmail(@email VARCHAR(50)) 
+    RETURNS INT 
+	AS
+    BEGIN
+	return( SELECT     Count(fd.Id)
+           FROM Passengers as p
+           INNER JOIN FlightDestinations as fd ON fd.PassengerId = p.Id
+		   WHERE p.Email= @email)
+  	END
+ GO
  
-)
+	 
+ SELECT dbo.udf_FlightDestinationsByEmail ('PierretteDunmuir@gmail.com')
 
-CREATE TABLE Addresses
-(
-Id INT PRIMARY KEY IDENTITY,
-Town VARCHAR(30) NOT NULL,
-Country NVARCHAR(30) NOT NULL,
-Streat NVARCHAR(100) NOT NULL,
-ZIP NVARCHAR(20) NOT NULL
-)
-
-CREATE TABLE Clients
-(
-Id INT PRIMARY KEY IDENTITY,
-FirstName NVARCHAR(30) NOT NULL,
-LastName  NVARCHAR(30) NOT NULL,
-Email NVARCHAR(50) NOT NULL,
-AddressId INT FOREIGN KEY REFERENCES Addresses(Id) NOT NULL
-)
-
-CREATE TABLE ClientsCigars
-(
-Id INT  FOREIGN KEY REFERENCES Clients(Id) ,
-CigarId INT FOREIGN KEY REFERENCES Cigars(Id),
-PRIMARY KEY(Id,CigarId)
-)
-
-
+ SELECT 
+ Email,
+ Count(fd.Id)
+ FROM Passengers as p
+ INNER JOIN FlightDestinations as fd ON fd.PassengerId = p.Id
+ GROUP BY Email
