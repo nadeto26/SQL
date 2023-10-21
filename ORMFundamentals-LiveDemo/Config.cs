1a5b43b2265c19e4be91db663e4680ebb57fe6d1
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-10-21 17:49:34 +0300

    Test

--- ORMFundamentals-LiveDemo/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿// ReSharper disable InconsistentNaming
-namespace ORMFundamentals_LiveDemo
-{
-    public class Config
-    {
-        public const string ConnectionString =
-            @"Server=.;Database=SoftUniCodeFirst;Integrated Security=True;";
-    }
-}
